a2a14aa4c6b3fc2e98146603b5aa58c3ae1351f8
hectortav <tavhector@gmail.com>
2022-10-06 02:52:31 +0300

    Initialize tailwindcss

---
+++ packages/ui/src/components/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+    return (
+        <button className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700">
+            Boop
+        </button>
+    )
+}
+
+export default Button
