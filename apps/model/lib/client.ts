44821d5857125840c576cb9c2b58dada87ed4ec2
hectortav <tavhector@gmail.com>
2022-10-09 17:09:36 +0300

    Fix environment variable problem

--- apps/model/lib/client.ts
+++ apps/model/lib/client.ts
@@ -2,8 +2,6 @@ import { PrismaClient, Prisma } from "@prisma/client"
 
 let config: Prisma.PrismaClientOptions = {}
 
-require("dotenv").config()
-
 if (process.env.NODE_ENV === "development") {
     config.log = ["query", "warn", "error"]
     console.log(`SQL log config: ${config.log}`)
--- packages/models/lib/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as prisma } from "./client"
-export * from "./hash"
-export * from "./movie-utils"
